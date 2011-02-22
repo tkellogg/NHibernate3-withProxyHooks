ce57d07f60b3c9c2dab999badce6d34cf631f598
Tim Kellogg <tkellogg@kpaonline.com>
2011-02-22 16:18:26 -0700

    added hooks to add interceptors and mixins I tested with regular project code, but no unit tests were added

--- src/NHibernate.ByteCode.Castle/ProxyFactory.cs
+++ src/NHibernate.ByteCode.Castle/ProxyFactory.cs
@@ -1,7 +1,9 @@
 using System;
 using Castle.DynamicProxy;
+using DP = Castle.DynamicProxy;
 using NHibernate.Engine;
 using NHibernate.Proxy;
+using System.Collections.Generic;
 
 namespace NHibernate.ByteCode.Castle
 {
@@ -14,6 +16,47 @@ namespace NHibernate.ByteCode.Castle
 		{
 			get { return ProxyGenerator; }
 		}
+
+        private static List<Func<System.Type, IEnumerable<DP.IInterceptor>>> s_interceptors = new List<Func<System.Type, IEnumerable<DP.IInterceptor>>>();
+        private DP.IInterceptor[] allInterceptors = null;
+        private static List<Func<System.Type, IEnumerable<System.Type>>> s_mixins = new List<Func<System.Type, IEnumerable<System.Type>>>();
+        private System.Type[] allMixins = null;
+
+        public static void AddInterceptors(Func<System.Type, IEnumerable<DP.IInterceptor>> interceptors)
+        {
+            s_interceptors.Add(interceptors);
+        }
+
+        public static void AddMixins(Func<System.Type, IEnumerable<System.Type>> mixins)
+        {
+            s_mixins.Add(mixins);
+        }
+
+        private IEnumerable<T> GetExtras<T>(IEnumerable<Func<System.Type, IEnumerable<T>>> creators, IEnumerable<T> plusThese)
+        {
+            /// Note on the plusThese argument: below NH expects their interceptor to be first at one point...not sure if this
+            /// actually matters so here is a mechanism to keep it modified as little as possible
+            if (plusThese != null)
+                foreach (var item in plusThese)
+                    yield return item;
+            foreach (var item in creators)
+                foreach (var i in item(PersistentClass))
+                    yield return i;
+        }
+
+        private void LazyInitAllExtras(LazyInitializer initializer)
+        {
+            if (allInterceptors == null)
+            {
+                var interc = new List<DP.IInterceptor>(GetExtras(s_interceptors, new DP.IInterceptor[] { initializer }));
+                allInterceptors = interc.ToArray();
+            }
+            if (allMixins == null)
+            {
+                var mix = new List<System.Type>(GetExtras(s_mixins, Interfaces));
+                allMixins = mix.ToArray();
+            }
+        }
 
 		/// <summary>
 		/// Build a proxy using the Castle.DynamicProxy library.
@@ -28,6 +71,7 @@ namespace NHibernate.ByteCode.Castle
 				var initializer = new LazyInitializer(EntityName, PersistentClass, id, GetIdentifierMethod,
 				                                            SetIdentifierMethod, ComponentIdType, session);
 
+                LazyInitAllExtras(initializer);
 				object generatedProxy = IsClassProxy
 				                        	? ProxyGenerator.CreateClassProxy(PersistentClass, Interfaces, initializer)
 				                        	: ProxyGenerator.CreateInterfaceProxyWithoutTarget(Interfaces[0], Interfaces,
